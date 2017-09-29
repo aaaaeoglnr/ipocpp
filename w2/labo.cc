9494bb47b3f01d9c0e9fa67b35bfeab00eac862f
Octo1a <oana.garlea@gmail.com>
2017-09-29 19:40:16 +0300

    Add assgnm2_1

---
+++ w2/labo.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Souris
+{
+  /*****************************************************
+    Compléter le code à partir d'ici
+  *******************************************************/
+	public:
+		Souris(double p, string c, unsigned int a = 0, 
+				unsigned int e = 36)
+			: poids(p), couleur(c), age(a), esperance_vie(e), clonee(false)
+		{
+			cout << "Une nouvelle souris !" << endl;
+		}
+
+		Souris(Souris const & autre)
+		: poids(autre.poids), couleur(autre.couleur), 
+		age(autre.age), 
+		esperance_vie(autre.esperance_vie * 4/5.0), clonee(true)
+		{
+			cout << "Clonage d'une souris !" << endl;	
+		}
+
+		~Souris(){
+			cout << "Fin d'une souris..." << endl;
+		}
+
+		void afficher()const {
+			cout << "Une souris " << couleur;
+ 			if (clonee) {
+ 				cout <<", clonee,";
+			}
+			cout << " de " << age << " mois et pesant "
+			<< poids << " grammes" << endl;
+		}
+
+		void vieillir(){
+			age++;
+			if (clonee and age > 0.5 * esperance_vie) {
+				couleur = "verte";
+			}
+		}
+
+		void evolue(){
+			while (age < esperance_vie) {
+				vieillir();
+			}
+		}
+
+
+	private:
+		double poids;
+		string couleur;
+		unsigned int age;
+		unsigned int esperance_vie;
+		bool clonee;
+
+  /*******************************************
+   * Ne rien modifier après cette ligne.
+   *******************************************/
+
+}; // fin de la classe Souris
+
+int main()
+{
+  Souris s1(50.0, "blanche", 2);
+  Souris s2(45.0, "grise");
+  Souris s3(s2);
+  // ... un tableau peut-être...
+  s1.afficher();
+  s2.afficher();
+  s3.afficher();
+  s1.evolue();
+  s2.evolue();
+  s3.evolue();
+  s1.afficher();
+  s2.afficher();
+  s3.afficher();
+  return 0;
+}
